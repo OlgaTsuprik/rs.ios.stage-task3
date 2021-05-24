6a35a02ef21f1191c5fb840538a9262edaad0802
Olga Tsuprik <tt.olia@mail.ru>
2021-05-24 17:54:19 +0300

    ADDED Tasks

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,29 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger counter = 0;
+
+        if (ratingArray == nil || ratingArray.count < 3) {
+            return counter;
+        }
+        
+        NSInteger i = 0;
+        NSInteger j = i + 1;
+        NSInteger k = j + 1;
+        while (i < ratingArray.count - 2) {
+            if ((ratingArray[i].intValue < ratingArray[j].intValue && ratingArray[j].intValue < ratingArray[k].intValue) ||
+                (ratingArray[i].intValue > ratingArray[j].intValue && ratingArray[j].intValue > ratingArray[k].intValue)) {
+                counter++;
+            }
+            if (j == ratingArray.count - 2) {
+                i++; j = i + 1; k = j + 1;
+            } else if (k == ratingArray.count - 1) {
+                j = j + 1 ; k = j + 1 ;
+            } else {
+                k++;
+            }
+        }
+        return counter;
 }
 
 @end
